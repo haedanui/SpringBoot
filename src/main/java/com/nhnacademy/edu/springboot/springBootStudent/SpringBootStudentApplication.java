7cb0be605c5826d53af6b025c498c86cdb12730e
haedanui <rhs9705kr@naver.com>
2024-02-19 16:00:45 +0900

    일단 다시

--- src/main/java/com/nhnacademy/edu/springboot/springBootStudent/SpringBootStudentApplication.java
+++ src/main/java/com/nhnacademy/edu/springboot/springBootStudent/SpringBootStudentApplication.java
@@ -1,13 +1,19 @@
 package com.nhnacademy.edu.springboot.springBootStudent;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class SpringBootStudentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStudentApplication.class, args);
+//		SpringApplication application = new SpringApplication(SpringBootStudentApplication.class);
+//		application.setBannerMode(Banner.Mode.OFF);
+//		application.run(args);
 	}
 
 }
